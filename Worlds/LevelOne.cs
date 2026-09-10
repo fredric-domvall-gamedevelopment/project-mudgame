68a3fa8a00736d205b7677aa4a7d08884ee2839f
Fredric Domvall <fredric.domvall@outlook.com>
2026-09-10 12:20:37 +0200

    implementing simple battle logic

--- Worlds/LevelOne.cs
+++ Worlds/LevelOne.cs
@@ -178,8 +178,40 @@ namespace MUD.Worlds
 
         private void Battle(Enemy enemy, Player player)
         {
-            Console.WriteLine($"Enemy: {enemy.Name} | Health: {enemy.Health} | Attack: {enemy.Attack} | Defence: {enemy.Defence}");
-            Console.WriteLine($"Player: {player.Name} | Health: {player.Health} | Attack: {player.Attack} | Defence: {player.Defence}");
+            do
+            {
+                Console.WriteLine($"Player: {player.Name} | HP: {player.Health}");
+                Console.WriteLine($"Enemy: {enemy.Name} | HP: {enemy.Health}");
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("1. Attack");
+                Console.WriteLine("3. Run");
+                char choice = Console.ReadKey().KeyChar;
+                switch (choice)
+                {
+                    case '1':
+                        enemy.Health -= player.Attack - enemy.Defence;
+                        Console.WriteLine($"You attack the {enemy.Name} for {player.Attack - enemy.Defence} damage!");
+                        player.Health -= enemy.Attack - player.Defence;
+                        Console.WriteLine($"The {enemy.Name} attacks you for {enemy.Attack - player.Defence} damage!");
+                        break;
+                    case '2':
+                        Console.WriteLine("You run away from the battle.");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice, please try again.");
+                        break;
+                }
+            } while (enemy.Health > 0 && player.Health > 0);
+
+            if(enemy.Health <= 0)
+            {
+                Console.WriteLine($"You have defeated the {enemy.Name}!");
+            }
+            else if (player.Health <= 0)
+            {
+                Console.WriteLine("You have been defeated!");
+                player.IsDead = true;
+            }
         }
     }
 }

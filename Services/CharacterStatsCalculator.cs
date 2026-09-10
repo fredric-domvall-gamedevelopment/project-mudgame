4a30ac32c7088b6a762adfe7d6da3efd09825841
Fredric Domvall <fredric.domvall@outlook.com>
2026-09-10 21:20:33 +0200

    creating simple attackcalculator method

---
+++ Services/CharacterStatsCalculator.cs
@@ -0,0 +1,12 @@
+﻿using MUD.Models;
+
+namespace MUD.Services;
+
+public class CharacterStatsCalculator
+{
+    public float CalculateCharacterAttack(Character character)
+    {
+        character.Attack = character.Stats.Strength;
+        return character.Attack;
+    }
+}

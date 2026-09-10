e3a0f6d13755213734f7484ee1f9df088c7f44a4
Fredric Domvall <fredric.domvall@outlook.com>
2026-09-10 11:00:34 +0200

    First gameplay setup

---
+++ Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MUD.Models
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Attack { get; set; }
+        public int Defence { get; set; }
+        public bool IsDead { get; set; }
+        public float Speed { get; set; }
+    }
+}
--- Worlds/LevelOne.cs
+++ Worlds/LevelOne.cs
@@ -1,28 +1,114 @@
 ﻿using MUD.Art;
+using MUD.Models;
 
 namespace MUD.Worlds
 {
     public class LevelOne
     {
-        public string Name;
-        public int Health;
-        public bool Dead;
+        Player player = new Player();
+
 
         public void StartGame()
         {
             Console.WriteLine("Welcome the magical worlds of MUDs");
             Console.WriteLine("Who are you?");
-            Name = Console.ReadLine();
-            Console.WriteLine("Oh, your name is " + Name);
+            player.Name = Console.ReadLine();
+            Console.WriteLine("Oh, your name is " + player.Name);
             Console.WriteLine("Not what I would have chosen, but it will do I suppose...");
-            Health = 100;
-            Console.WriteLine("You have " + Health + " HP");
-            Dead = false;
+            player.Health = 100;
+            Console.WriteLine("You have " + player.Health + " HP");
+            player.IsDead = false;
 
-            if(Dead)
+            if (player.IsDead)
                 Console.WriteLine("You are however, dead?");
             else
                 Console.WriteLine("You are alive? Good!");
+
+            PlayGame();
+        }
+
+        private void CreatePlayer()
+        {
+
+        }
+
+        private void PlayGame()
+        {
+            Console.WriteLine($"Player: {player.Name} | HP: {player.Health}");
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("1. Go to the Sea");
+            Console.WriteLine("2. Go to the Forest");
+            Console.WriteLine("3. Go to the Tavern");
+
+            Char choice = Console.ReadKey().KeyChar;
+
+            switch (choice)
+            {
+                case '1':
+                    Console.WriteLine("You chose to go to the Sea");
+                    GoToSea();
+                    PlayGame();
+                    break;
+                case '2':
+                    Console.WriteLine("You chose to go to the Mountains");
+                    GoToMountains();
+                    PlayGame();
+                    break;
+                case '3':
+                    Console.WriteLine("You chose to go to the Tavern");
+                    GoToTavern();
+                    PlayGame();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice, please try again.");
+                    PlayGame();
+                    break;
+            }
+        }
+
+        private void GoToSea()
+        {
+            Sea sea = new Sea();
+            sea.ShowGraphic();
+            Console.WriteLine("you meet a seamonster, you get scared and run away. you loose 10 Hp");
+            player.Health -= 10;
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        private void GoToMountains()
+        {
+            Mountains mountain = new Mountains();
+            mountain.ShowGraphic();
+            Console.WriteLine("you meet a goblin, you get scared and run away. you loose 10 Hp");
+            player.Health -= 10;
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        private void GoToTavern()
+        {
+            Console.WriteLine("----------------THE TAVERN-----------------");
+            Console.WriteLine("you enter the tavern, it is warm and cozy");
+            Console.WriteLine("you meet a bartender, he offers you a room to rest in");
+            Console.WriteLine("do you accept his kind offer?  \n  (press y to accept, any other key to decline)");
+            char choice = Console.ReadKey().KeyChar;
+            if (choice == 'y')
+            {
+                Console.WriteLine("You accept the offer and rest in the tavern.");
+                Console.WriteLine("You recover to full health.");
+                player.Health = 100;
+            }
+            else
+            {
+                Console.WriteLine("You decline the offer and continue on your way.");
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            Console.Clear();
         }
     }
 }

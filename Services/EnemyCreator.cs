6ce2306273680bedf5f4e88172ead7e6937b954f
Fredric Domvall <fredric.domvall@outlook.com>
2026-09-11 00:34:06 +0200

    connect statcalculator to enemycreator

--- Services/EnemyCreator.cs
+++ Services/EnemyCreator.cs
@@ -1,5 +1,6 @@
 ﻿using MUD.Models;
 using MUD.Models.Enums;
+using MUD.Services;
 
 namespace MUD.Services;
 
@@ -8,30 +9,43 @@ public class EnemyCreator
     public Enemy CreateEnemy(EnemyType enemyType)
     {
         Enemy enemy = new Enemy();
+        CharacterStatsCalculator characterStatsCalculator = new CharacterStatsCalculator();
         switch(enemyType)
         {
             case EnemyType.Goblin:
                 enemy.Name = "Goblin";
                 enemy.MaxHealth = 50f;
+                enemy.Stats.Strength = 7f;
+                enemy.Stats.Dexterity = 5f;
+                enemy.Stats.Endurance = 3f;
+                enemy.Attack = characterStatsCalculator.CalculateCharacterAttack(enemy);
+                enemy.Defense = characterStatsCalculator.ClalculateCharacterDefense(enemy);
+                enemy.MaxHealth = characterStatsCalculator.CalculateCharacterHealth(enemy);
                 enemy.Health = enemy.MaxHealth;
-                enemy.Attack = 10f;
-                enemy.Defense = 5f;
                 enemy.IsDead = false;
                 break;
             case EnemyType.Orc:
                 enemy.Name = "Orc";
-                enemy.MaxHealth = 80f;
+                enemy.MaxHealth = 60f;
+                enemy.Stats.Strength = 8f;
+                enemy.Stats.Dexterity = 6f;
+                enemy.Stats.Endurance = 4f;
+                enemy.Attack = characterStatsCalculator.CalculateCharacterAttack(enemy);
+                enemy.Defense = characterStatsCalculator.ClalculateCharacterDefense(enemy);
+                enemy.MaxHealth = characterStatsCalculator.CalculateCharacterHealth(enemy);
                 enemy.Health = enemy.MaxHealth;
-                enemy.Attack = 15f;
-                enemy.Defense = 5f;
                 enemy.IsDead = false;
                 break;
             case EnemyType.Troll:
                 enemy.Name = "Troll";
-                enemy.MaxHealth = 80f;
+                enemy.MaxHealth = 70f;
+                enemy.Stats.Strength = 9f;
+                enemy.Stats.Dexterity = 7f;
+                enemy.Stats.Endurance = 5f;
+                enemy.Attack = characterStatsCalculator.CalculateCharacterAttack(enemy);
+                enemy.Defense = characterStatsCalculator.ClalculateCharacterDefense(enemy);
+                enemy.MaxHealth = characterStatsCalculator.CalculateCharacterHealth(enemy);
                 enemy.Health = enemy.MaxHealth;
-                enemy.Attack = 20f;
-                enemy.Defense = 8f;
                 enemy.IsDead = false;
                 break;
             default:

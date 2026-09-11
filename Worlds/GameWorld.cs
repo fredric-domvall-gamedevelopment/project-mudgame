c14c92b4788d8f2e32c6f5ec523c9b9bf275020b
Fredric Domvall <fredric.domvall@outlook.com>
2026-09-11 12:35:09 +0200

    moving createplayer to new class

--- Worlds/GameWorld.cs
+++ Worlds/GameWorld.cs
@@ -5,94 +5,17 @@ namespace MUD.Worlds
 {
     public class GameWorld
     {
-        CharacterStatsCalculator calculator = new CharacterStatsCalculator();
         Player player = new Player();
+        PlayerCreator playerCreator = new PlayerCreator();
 
 
         public void StartGame()
         {
             Console.WriteLine("Welcome the magical worlds of MUDs \n");
 
-            CreatePlayer();
+            player = playerCreator.CreatePlayer();
 
             PlayGame();
-        }
-
-        private void CreatePlayer()
-        {
-            int SkillPoints = 20;
-            player.Stats.Strength = 0f;
-            player.Stats.Dexterity = 0f;
-            player.Stats.Endurance = 0f;
-            player.Attack = 0f;
-            player.Defense = 0f;
-            player.MaxHealth = 50f;
-            player.IsDead = false;          
-
-            Console.WriteLine("-----Create your character-----\n");
-            Console.WriteLine("What is your name?");
-
-            player.Name = Console.ReadLine()!;
-            if(String.IsNullOrEmpty(player.Name))
-            {
-                while (String.IsNullOrEmpty(player.Name))
-                {
-                    Console.WriteLine("Please enter a valid name");
-                    player.Name = Console.ReadLine()!;
-                }
-            }
-            Console.Clear();
-
-            do
-            {
-                Console.WriteLine("-----Set your Attack & Defence stats-----\n");
-                Console.WriteLine($"You have {SkillPoints} points to spend\n");
-                Console.WriteLine($"1. Increase Strength  | Current STR: {player.Stats.Strength}");
-                Console.WriteLine($"2. Increase Dexterity | Current DEX: {player.Stats.Dexterity}");
-                Console.WriteLine($"3. Increase Endurance | Current END: {player.Stats.Endurance}");
-
-                char choice = Console.ReadKey().KeyChar;
-                Console.Clear();
-
-                switch (choice)
-                {
-                    case '1':
-                        player.Stats.Strength++;
-                        SkillPoints--; 
-                        break;
-                    case '2':
-                        player.Stats.Dexterity++;
-                        SkillPoints--;
-                        break;
-                    case '3':
-                        player.Stats.Endurance++;
-                        SkillPoints--;
-                        break;
-                    default:
-                        
-                        Console.WriteLine("Invalid choice, please try again.\n");
-                        break;
-                }
-                if(SkillPoints == 0)
-                {
-                    player.Attack = calculator.CalculateCharacterAttack(player);
-                    player.Defense = calculator.ClalculateCharacterDefense(player);
-                    player.MaxHealth = calculator.CalculateCharacterHealth(player);
-                    player.Health = player.MaxHealth;
-
-                    Console.WriteLine($"Character created! \n" +
-                        $"Name: [{player.Name}] \n" +
-                        $"STR: {player.Stats.Strength} || DEX: {player.Stats.Dexterity} || END: {player.Stats.Endurance}\n" +
-                        $"HP: {player.Health}/{player.MaxHealth} || Attack: {player.Attack} || Defense: {player.Defense}");
-                    Console.WriteLine("Are you happy with your character? (press n to restart, any other key to continue)");
-
-                    choice = Console.ReadKey().KeyChar;
-                    Console.Clear();
-
-                    if (choice == 'N' || choice == 'n')
-                        StartGame();
-                }
-            } while (SkillPoints > 0);
         }
 
         private void PlayGame()

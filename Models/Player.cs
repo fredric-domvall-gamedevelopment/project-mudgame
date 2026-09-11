c524d56e5619fa05b3b9b875767d4a376edd2a67
Fredric Domvall <fredric.domvall@outlook.com>
2026-09-11 18:09:00 +0200

    adding properties to player model

--- Models/Player.cs
+++ Models/Player.cs
@@ -2,6 +2,11 @@
 
 public class Player : Character
 {
+    public int CurrentXp { get; set; }
+    public int NextLevelXp { get; set; }
+    public int Gold { get; set; }
+    public int SkillPoints { get; set; }
+
     //Hitta Lösning för inventory
 
     //Hitta lösning för equipment

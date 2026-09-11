ac4f69bc560d890766785846f6c9ca63eb43203d
Fredric Domvall <fredric.domvall@outlook.com>
2026-09-11 10:31:14 +0200

    moving tavern into new town class

--- Worlds/LevelOne.cs
+++ Worlds/LevelOne.cs
@@ -127,7 +127,7 @@ namespace MUD.Worlds
                     break;
                 case '3':
                     Console.WriteLine("You chose to go to the Tavern");
-                    GoToTavern();
+                    GoToTavern(player);
                     PlayGame();
                     break;
                 default:
@@ -158,29 +158,10 @@ namespace MUD.Worlds
             Console.Clear();
         }
 
-        private void GoToTavern()
+        private void GoToTavern(Player player)
         {
-            Console.WriteLine("----------------THE TAVERN-----------------\n");
-            Console.WriteLine("you enter the tavern, it is warm and cozy");
-            Console.WriteLine("you meet a bartender, he offers you a room to rest in");
-            Console.WriteLine("do you accept his kind offer?  \n  (press y to accept, any other key to decline)");
-
-            char choice = Console.ReadKey().KeyChar;
-            Console.Clear();
-            if (choice == 'y')
-            {
-                Console.WriteLine("You accept the offer and rest in the tavern.");
-                Console.WriteLine("You recover to full health.");
-                player.Health = player.MaxHealth;
-            }
-            else
-            {
-                Console.WriteLine("You decline the offer and continue on your way.");
-            }
-
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
-            Console.Clear();
+            Town town = new Town();
+            town.EnterTown(player);
         }
 
         private Enemy CreateEnemy(Enemy enemy)
---
+++ Worlds/Town.cs
@@ -0,0 +1,38 @@
+﻿using MUD.Models;
+
+namespace MUD.Worlds;
+public class Town
+{
+    public void EnterTown(Player player)
+    {
+        Console.WriteLine("You have entered the town.");
+        Console.WriteLine("only a Tavern remain standing. Maybe can rest here and recover your health. \n");
+        Tavern(player);
+    }
+
+    private void Tavern(Player player)
+    {
+        Console.WriteLine("----------------THE TAVERN-----------------\n");
+        Console.WriteLine("you enter the tavern, it is warm and cozy");
+        Console.WriteLine("you meet a bartender, he offers you a room to rest in");
+        Console.WriteLine("do you accept his kind offer?  \n  (press y to accept, any other key to decline)");
+
+        char choice = Console.ReadKey().KeyChar;
+        Console.Clear();
+        if (choice == 'y')
+        {
+            Console.WriteLine("You accept the offer and rest in the tavern.");
+            Console.WriteLine("You recover to full health.");
+            player.Health = player.MaxHealth;
+        }
+        else
+        {
+            Console.WriteLine("You decline the offer and continue on your way.");
+        }
+
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+        Console.Clear();
+    }
+
+}

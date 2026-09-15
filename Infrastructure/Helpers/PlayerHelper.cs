6f000aaf3e591380b9f800b5491d110fbbd0bed6
Fredric Domvall <fredric.domvall@outlook.com>
2026-09-16 00:20:49 +0200

    creating static bool helper to check if player is dead

---
+++ Infrastructure/Helpers/PlayerHelper.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.Models;
+
+namespace Infrastructure.Helpers;
+
+public static class PlayerHelper
+{
+    public static bool PlayerIsDead(Player player)
+    {
+        if (player.Health <= 0)
+            player.IsDead = true;
+        
+        return player.IsDead;
+    }
+}

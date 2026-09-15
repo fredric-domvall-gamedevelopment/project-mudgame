6c856e03fe602e5bba6d4e1bc8aa9ac0fc603ce6
Fredric Domvall <fredric.domvall@outlook.com>
2026-09-15 09:57:20 +0200

    creating static consolehelper class

---
+++ Helpers/ConsoleHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace MUD.Helpers;
+public static class ConsoleHelper
+{
+    public static void Continue()
+    {
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+        Console.Clear();
+    }
+}

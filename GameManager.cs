f5db89edf87ca0816775512586372bc04d371e29
Fredric Domvall <fredric.domvall@outlook.com>
2026-09-15 13:03:04 +0200

    creating game manager class to add menus

---
+++ GameManager.cs
@@ -0,0 +1,11 @@
+﻿using MUD.Worlds;
+
+namespace MUD;
+public class GameManager
+{
+    public void StartMenu()
+    {
+        GameWorld levelOne = new GameWorld();
+        levelOne.StartGame();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,8 +7,8 @@ namespace MUD
         static void Main(string[] args)
         {
             Console.Title = "MUD - The Magical World";
-            GameWorld levelOne = new GameWorld();
-            levelOne.StartGame();
+            GameManager gameManager = new GameManager();
+            gameManager.StartMenu();
         }
     }
 }

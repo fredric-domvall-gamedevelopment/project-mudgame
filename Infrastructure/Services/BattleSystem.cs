bdc3367030e75e0cd93b07361fe343f9b2551957
Fredric Domvall <fredric.domvall@outlook.com>
2026-09-16 01:08:22 +0200

    implementing new helper and changing its type

--- Infrastructure/Services/BattleSystem.cs
+++ Infrastructure/Services/BattleSystem.cs
@@ -20,28 +20,13 @@ public class BattleSystem
                 enemy.Health -= playerDamage;
                 battleInformation.Add($"You attack the {enemy.Name} for {playerDamage} damage!");
 
-                if (enemy.Health <= 0)
-                {
-                    battleInformation.Add($"You have defeated the {enemy.Name}!");
-                    battleResult = BattleResult.EnemyDead;
-                    enemy.IsDead = true;
-
-                    return new ResultResponse<BattleResult> { IsSuccess = true, Data = battleResult, Information = battleInformation };
-                }
+                PlayerHelper.IsPlayerDead(player);
 
                 float enemyDamage = Math.Max(enemy.Attack - player.Defense, 0);
                 player.Health -= enemyDamage;
                 battleInformation.Add($"The {enemy.Name} attacks you for {enemyDamage} damage!");
 
-                if (player.Health <= 0)
-                {
-                    battleInformation.Add("You have been defeated!");
-                    battleResult = BattleResult.PlayerDead;
-                    
-                    PlayerHelper.PlayerIsDead(player);
-
-                    return new ResultResponse<BattleResult> { IsSuccess = true, Data = battleResult, Information = battleInformation };
-                }
+                PlayerHelper.IsPlayerDead(player);
 
                 battleInformation.Add("The battle continues...");
                 battleResult = BattleResult.Continue;
@@ -55,15 +40,7 @@ public class BattleSystem
                 player.Health -= enemyDamage;
                 battleInformation.Add($"The {enemy.Name} attacks you for {enemyDamage} damage!");
 
-                if (player.Health <= 0)
-                {
-                    battleInformation.Add("You have been defeated!");
-
-                    battleResult = BattleResult.PlayerDead;
-                    player.IsDead = true;
-
-                    return new ResultResponse<BattleResult> { IsSuccess = true, Data = battleResult, Information = battleInformation };
-                }
+                PlayerHelper.IsPlayerDead(player);
 
                 battleResult = BattleResult.Continue;
 
@@ -86,15 +63,8 @@ public class BattleSystem
                 player.Health -= enemyDamage;
 
                 battleInformation.Add($"The {enemy.Name} attacks you for {enemyDamage} damage!");
-                
-                if (player.Health <= 0)
-                {
-                    battleInformation.Add("You have been defeated!");
-                    battleResult = BattleResult.PlayerDead;
-                    player.IsDead = true;
 
-                    return new ResultResponse<BattleResult> { IsSuccess = true, Data = battleResult, Information = battleInformation };
-                }
+                PlayerHelper.IsPlayerDead(player);
 
                 battleResult = BattleResult.Continue;
 

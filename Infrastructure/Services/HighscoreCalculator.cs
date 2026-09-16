403206e07313a8306b07eb2201b5673c34a0f818
Fredric Domvall <fredric.domvall@outlook.com>
2026-09-16 15:03:18 +0200

    creating small statcalculator method

---
+++ Infrastructure/Services/HighscoreCalculator.cs
@@ -0,0 +1,12 @@
+﻿using Infrastructure.Models;
+
+namespace Infrastructure.Services;
+public class HighscoreCalculator
+{
+    public Player CalculateHighscore(Player player)
+    {
+        player.Score = (int) (player.Stats.Strength + player.Stats.Dexterity + player.Stats.Endurance);
+
+        return player;
+    }
+}

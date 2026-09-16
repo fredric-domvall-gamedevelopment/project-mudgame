b7aa88b9e3dc8d7e62bb35c4b50bc6fb290b1e10
Fredric Domvall <fredric.domvall@outlook.com>
2026-09-16 16:39:29 +0200

    created filesources for json files

---
+++ Infrastructure/Configurations/FileSources.cs
@@ -0,0 +1,13 @@
+﻿namespace Infrastructure.Configurations;
+public class FileSources
+{
+    public FileSources()
+    {
+        var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        var projectRoot = Path.GetFullPath(Path.Combine(baseDirectory, @"..", "..", ".."));
+
+        HighscoreFileSource = Path.Combine(projectRoot, "Infrastructure", "JsonFiles", "highscore.json");
+    }
+    public string HighscoreFileSource { get; set; }
+
+}

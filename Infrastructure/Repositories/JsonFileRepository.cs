7596b9e4929a1c02cc8c10736573c860300e366a
Fredric Domvall <fredric.domvall@outlook.com>
2026-09-16 16:08:53 +0200

    sending back empty generic lists if validation passes

--- Infrastructure/Repositories/JsonFileRepository.cs
+++ Infrastructure/Repositories/JsonFileRepository.cs
@@ -10,17 +10,17 @@ public class JsonFileRepository<T>
         try
         {
             if(!File.Exists(filePath))
-                return new ResultResponse<List<T>> { IsSuccess = false, Information = new List<string> { "JSON file not found." } };
+                return new ResultResponse<List<T>> { IsSuccess = false, Data = new List<T>(), Information = new List<string> { "JSON file not found." } };
 
             var jsonData = await File.ReadAllTextAsync(filePath);
 
             if (string.IsNullOrWhiteSpace(jsonData))
-                return new ResultResponse<List<T>> { IsSuccess = false, Information = new List<string> { "JSON file is empty." } };
+                return new ResultResponse<List<T>> { IsSuccess = false, Data = new List<T>(), Information = new List<string> { "JSON file is empty." } };
 
             var listData = JsonSerializer.Deserialize<List<T>>(jsonData);
 
             if(listData == null)
-                return new ResultResponse<List<T>> { IsSuccess = false, Information = new List<string> { "Failed to deserialize JSON data." } };
+                return new ResultResponse<List<T>> { IsSuccess = false, Data = new List<T>(), Information = new List<string> { "Failed to deserialize JSON data." } };
 
             return new ResultResponse<List<T>>
             {

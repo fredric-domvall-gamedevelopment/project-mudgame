4f65423c763db9fbdba39f88d2a824092ccbce90
Fredric Domvall <fredric.domvall@outlook.com>
2026-09-16 20:55:16 +0200

    sorting misplaced json filesaves

--- Infrastructure/Configurations/FileSources.cs
+++ Infrastructure/Configurations/FileSources.cs
@@ -4,7 +4,7 @@ public class FileSources
     public FileSources()
     {
         var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
-        var projectRoot = Path.GetFullPath(Path.Combine(baseDirectory, @"..", "..", ".."));
+        var projectRoot = Path.GetFullPath(Path.Combine(baseDirectory, @"..", "..", "..",".."));
 
         HighscoreFileSource = Path.Combine(projectRoot, "Infrastructure", "JsonFiles", "highscore.json");
     }

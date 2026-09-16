9d60b3d0ac17a24e1fdf8580d9d95c07cf205bcf
Fredric Domvall <fredric.domvall@outlook.com>
2026-09-16 14:39:51 +0200

    add create and read methods + list to highscore service

--- Infrastructure/Services/ScoreSystem.cs
+++ Infrastructure/Services/ScoreSystem.cs
@@ -1,4 +1,17 @@
-﻿namespace Infrastructure.Services;
+﻿using Infrastructure.Models;
+
+namespace Infrastructure.Services;
 public class ScoreSystem
 {
+    private readonly List<Player> _playerHighscore = new List<Player>();
+
+    public void AddPlayerToHighscoreList(Player player)
+    {
+        _playerHighscore.Add(player);
+    }
+
+    public List<Player> GetHighscoreList()
+    {
+        return _playerHighscore;
+    }
 }

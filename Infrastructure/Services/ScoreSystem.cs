32bb400360fa1972c705d51d2146aea255d5398f
Fredric Domvall <fredric.domvall@outlook.com>
2026-09-16 16:31:22 +0200

    connecting jeson file repo to scoresystem

--- Infrastructure/Services/ScoreSystem.cs
+++ Infrastructure/Services/ScoreSystem.cs
@@ -1,20 +1,30 @@
 ﻿using Infrastructure.Models;
+using Infrastructure.Repositories;
 
 namespace Infrastructure.Services;
 public class ScoreSystem
 {
     private readonly List<Player> _playerHighscore = new List<Player>();
+    private readonly JsonFileRepository<Player> _jsonFileRepository;
+    private readonly string _highscoreFilePath;
 
+    public ScoreSystem(JsonFileRepository<Player> jsonFileRepository, string highscoreFilePath)
+    {
+        _jsonFileRepository = jsonFileRepository;
+        _highscoreFilePath = highscoreFilePath;
+    }
     public void CalculateHighscore(Player player)
     {
         player.Score = (int)(player.Stats.Strength + player.Stats.Dexterity + player.Stats.Endurance);
     }
 
-    public void AddPlayerToHighscoreList(Player player)
+    public async Task<Player>AddPlayerToHighscoreList(Player player)
     {
         CalculateHighscore(player);
 
         _playerHighscore.Add(player);
+        await _jsonFileRepository.WriteToJsonAsync(_highscoreFilePath, _playerHighscore);
+        return player;
     }
 
     public List<Player> GetHighscoreList()

99b1d37ce77328027d9c22e50469f979c21bc138
Fredric Domvall <fredric.domvall@outlook.com>
2026-09-16 15:50:52 +0200

    creating basic repository to read from jsonfile

---
+++ Infrastructure/Repositories/JsonFileRepository.cs
@@ -0,0 +1,31 @@
+﻿using Infrastructure.Models;
+using System.Text.Json;
+
+namespace Infrastructure.Repositories;
+
+public class JsonFileRepository<T>
+{
+    public async Task<ResultResponse<List<T>>> ReadFromJsonAsync(string filePath)
+    {
+        try
+        {
+            var jsonData = await File.ReadAllTextAsync(filePath);
+
+            var listData = JsonSerializer.Deserialize<List<T>>(jsonData);
+
+            return new ResultResponse<List<T>>
+            {
+                IsSuccess = true,
+                Data = listData
+            };
+        }
+        catch (Exception ex)
+        {
+            return new ResultResponse<List<T>>
+            {
+                IsSuccess = false,
+                Information = new List<string> { $"Error reading from JSON file: {ex.Message}" }
+            };
+        }
+    }
+}

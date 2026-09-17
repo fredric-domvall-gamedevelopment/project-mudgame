c1f4a6449baa47d4432d0502d79d6587c2cdede0
Fredric Domvall <fredric.domvall@outlook.com>
2026-09-17 12:34:38 +0200

    added stats display and set skills to player meny - show player stats

--- MudGame/GameManager.cs
+++ MudGame/GameManager.cs
@@ -68,8 +68,38 @@ public class GameManager
         switch (choice)
         {
             case '1':
-                Console.WriteLine("Nothing to show yet, will be added soon");
-                ConsoleHelper.Continue();
+                Console.WriteLine(
+                    "--- Player Stats ---    {0,-10} {1,-15} {2,-10} {3}\n",
+                    "Name:", player.Name,
+                    "Level:", player.Level,
+                    "Score:", player.Score);
+
+                Console.WriteLine(
+                    "{0,-12} {1,-12} {2,-12} {3,-12}",
+                    "Health:", $"{player.Health} / {player.MaxHealth}",
+                    "Strength:", player.Stats.Strength);
+
+                Console.WriteLine(
+                    "{0,-12} {1,-12} {2,-12} {3,-12}",
+                    "Attack:", player.Attack,
+                    "Dexterity:", player.Stats.Dexterity);
+
+                Console.WriteLine(
+                    "{0,-12} {1,-12} {2,-12} {3,-12}\n",
+                    "Defense:", player.Defense,
+                    "Endurance:", player.Stats.Endurance);
+
+                if(player.SkillPoints > 0)
+                {
+                    Console.WriteLine($"You have {player.SkillPoints} skill points to spend. Spend them now?\n" +
+                        $"Press Y to spend, any other key to skip.");
+                    choice = Console.ReadKey(true).KeyChar;
+
+                    if(choice == 'Y' || choice == 'y')
+                        playerCreator.SetSkillPoints(player);
+                    else
+                        ConsoleHelper.Continue();
+                }
 
                 PlayerMenu(player);
                 break;
